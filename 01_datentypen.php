a71cc6461cdd827c06d3c3f407b1b86fc47e1c0b
Martin Zauner <zauner.martin1980@gmail.com>
2024-08-08 16:04:27 +0200

    Line 4 added

--- 01_datentypen.php
+++ 01_datentypen.php
@@ -5,3 +5,9 @@ $version = 8;
 echo "ich nutze php Version \$version";
 
 echo "die php-Version ist $version";
+
+$zahl1 = 10;
+$zahl2 = 42;
+$summe = $zahl1 + $zahl2;
+
+echo "die Summe aus $zahl1 und $zahl2 ist $summe";
